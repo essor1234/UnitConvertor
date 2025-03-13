fa5be06654f31d759b1a2052f60ab37f529ff548
essor1234 <manhxuanquy2004@gmail.com>
2025-03-13 22:59:42 +0700

    First commit. Create some UI for the app

---
+++ app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.unitconvertor;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        Spinner Unit Setting
+        Spinner firstUnit = findViewById(R.id.firstUnit);
+        Spinner secondUnit = findViewById(R.id.secondUnit);
+        EditText firstNum = findViewById(R.id.firstNum);
+        EditText secondNum = findViewById(R.id.secondNum);
+
+//        Edge To Edge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onConvert(View view){
+
+    }
+}
